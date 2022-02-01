bd6836b0f615b8913f5c52e13c46f77ec2509b85
vangeliq <valeryangelique2001@gmail.com>
2022-01-31 17:18:11 -0800

    refactored adding users, and added a printUserList method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,30 @@
-//import org.keycloak.admin.client.resource.UserResource;
-//import org.keycloak.representations.idm.ClientRepresentation;
-//import org.keycloak.representations.idm.CredentialRepresentation;
-//import org.keycloak.representations.idm.RoleRepresentation;
-
-import au.com.bytecode.opencsv.CSVReader;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.FileNotFoundException;
-
-//https://www.javatpoint.com/how-to-read-csv-file-in-java
-import java.io.InputStream;
+import java.io.FileReader;
+import au.com.bytecode.opencsv.CSVReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-public class Main {
 
-    public static void main(String[] args) throws Exception  {
+public class Main {
+    public static void main(String[] args) throws Exception {
         String inputFile = "C:\\Users\\valery.angelique\\IdeaProjects\\create-healthnet-test-users\\src\\main\\java\\data\\input.csv";
         String serverUrl = "http://localhost:8080/auth";
         String realm = "realm15";
         String clientId = "clientTest";
         String clientSecret = "246f56b4-c34e-4d09-b72b-278d33f489bb";
-        // User "idm-admin" needs at least "manage-users, view-clients, view-realm, view-users" roles for "realm-management"
+
         Keycloak keycloak = KeycloakBuilder.builder() //
                 .serverUrl(serverUrl) //
                 .realm(realm) //
@@ -42,66 +33,53 @@ public class Main {
                 .clientSecret(clientSecret) //
                 .build();
 
-        		// Client "idm-client" needs service-account with at least "manage-users, view-clients, view-realm, view-users" roles for "realm-management"
-//		Keycloak keycloak = KeycloakBuilder.builder() //
-//				.serverUrl(serverUrl) //
-//				.realm(realm) //
-//				.grantType(OAuth2Constants.CLIENT_CREDENTIALS) //
-//				.clientId(clientId) //
-//				.clientSecret(clientSecret).build();
+        RealmResource realmResource = keycloak.realm(realm);
+        UsersResource usersResource = realmResource.users();
 
-        CSVReader reader = new CSVReader(new FileReader(inputFile), ',' , '"' , 0);
+        CSVReader reader = new CSVReader(new FileReader(inputFile), ',', '"', 0);
         List<String[]> allRows = reader.readAll();
 
-        for(String[] row : allRows){
-            UserRepresentation user = makeUser(row);
+        for (String[] row : allRows) {
+            addUser(row,usersResource);
 
-                    // Get realm
-        RealmResource realmResource = keycloak.realm(realm);
-        UsersResource usersRessource = realmResource.users();
-
-                    // Create user (requires manage-users role)
-        Response response = usersRessource.create(user);
-        System.out.printf("Repsonse: %s %s%n", response.getStatus(), response.getStatusInfo());
-        System.out.println(response.getLocation());
-        String userId = CreatedResponseUtil.getCreatedId(response);
-
-        System.out.printf("User created with userId: %s%n", userId);
         }
+        printUserList(usersResource);
 
-
-    // Define user
-//        UserRepresentation user = new UserRepresentation();
-//        user.setEnabled(true);
-//        user.setUsername("tester123");
-//        user.setFirstName("First");
-//        user.setLastName("Lalasterst");
-//        user.setEmail("tom+tester1@.local");
-//        user.setAttributes(Collections.singletonMap("origin", Arrays.asList("demo")));
-//
-//        // Get realm
-//        RealmResource realmResource = keycloak.realm(realm);
-//        UsersResource usersRessource = realmResource.users();
-//
-//        // Create user (requires manage-users role)
-//        Response response = usersRessource.create(user);
-//        System.out.printf("Repsonse: %s %s%n", response.getStatus(), response.getStatusInfo());
-//        System.out.println(response.getLocation());
-//        String userId = CreatedResponseUtil.getCreatedId(response);
-//
-//        System.out.printf("User created with userId: %s%n", userId);
     }
 
-    public static UserRepresentation makeUser(String[] userInfo){
-        String password = userInfo[1];
-        String idp = userInfo[2];
-        String role1 = userInfo[3];
-        String role2 = userInfo[4];
-
+    /*
+    userinfo is: username, password, IDP, role1, role2
+    */
+    private static void addUser(String[] userInfo, UsersResource usersResource) {
         UserRepresentation user = new UserRepresentation();
         user.setEnabled(true);
         user.setUsername(userInfo[0]);
         user.setAttributes(Collections.singletonMap("origin", Arrays.asList("demo")));
-        return user;
+
+        Response response = usersResource.create(user);
+        System.out.printf("Response: %s %s%n", response.getStatus(), response.getStatusInfo());
+        System.out.println(response.getLocation());
+        String userId = CreatedResponseUtil.getCreatedId(response);
+        System.out.printf("User created with userId: %s%n", userId);
+    }
+
+
+    public static void setUserPassword(String userId) {
+        //todo: implement this
+    }
+
+    ;
+
+    public static void addRoleToUser() {
+
+    }
+
+    public static void printUserList(UsersResource usersResource) {
+        List<UserRepresentation> users = usersResource.list();
+        for (UserRepresentation user : users) {
+            String name = user.getUsername();
+            String id = user.getId();
+            System.out.println(name + " id: " + id);
+        }
     }
 }
