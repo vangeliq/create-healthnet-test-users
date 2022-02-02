e5295ab2381313fb5fec7fc189293e9d2d57f18f
vangeliq <valeryangelique2001@gmail.com>
2022-02-02 12:07:03 -0800

    moved methods into the User class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,12 @@ import com.github.cliftonlabs.json_simple.JsonArray;
 import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
 import org.keycloak.OAuth2Constants;
-import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.*;
 import org.keycloak.representations.idm.*;
 
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
 import java.io.*;
-
-import au.com.bytecode.opencsv.CSVReader;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -46,8 +41,9 @@ public class Main {
         init();
 
         for (User user : users.values()) {
-//            addUserToKeyCloak(user.getUserRepresentation());
-            addUserClientRoles(user);
+            user.addToKeyCloak(usersResource);
+            user.addClientRolesInKeyCloak(realmResource);
+//            user.deleteFromKeycloak(usersResource);
         }
     }
 
@@ -106,7 +102,7 @@ public class Main {
 
                 Map<String,JsonArray> applications = (Map<String, JsonArray>) jsonUser.get("applications");
                 applications.forEach((client, roles) -> {
-                    roles.forEach(role -> user.addClientRoles(client, (String) role));
+                    roles.forEach(role -> user.recordClientRoles(client, (String) role));
                 });
 
                 try{
@@ -123,93 +119,7 @@ public class Main {
         return result;
     }
 
-    // adds user to realm, and updates id value on the user's UserRepresentation
-    private static void addUserToKeyCloak(UserRepresentation user) {
-        try {
-            Response response = usersResource.create(user);
-            System.out.printf("Response: %s %s%n", response.getStatus(), response.getStatusInfo());
-            System.out.println(response.getLocation());
-            String userId = CreatedResponseUtil.getCreatedId(response);
-            user.setId(userId);
-            System.out.printf("User created with userId: %s%n", userId);
-        }catch(WebApplicationException e){
-            e.printStackTrace();
-        }
-    }
-
-    // todo: should throw exception instead? should it ignore if not found?
-    private static void deleteUserFromKeyCloak(String userName) {
-        try {
-            String userID = getUserID(userName);
-            usersResource.delete(userID);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // todo: what to do if user not found
-    private static void addUserClientRoles(User user){
-        try{//try looking for the user
-            String userID = getUserID(user);
-            HashMap<String, Set<String>> clientRoles =  user.getClientRoles();
-
-            for(String clientID: clientRoles.keySet()){
-                String clientUUID = getClientUUID(clientID);
-
-                ClientResource clientResource = realmResource.clients().get(clientUUID);
-                List<RoleRepresentation> rolesToAdd = getRolesToAdd(clientRoles.get(clientID),clientResource);
-
-                UserResource userResource = realmResource.users().get(userID);
-                RoleMappingResource roleMappingResource = userResource.roles();
-                RoleScopeResource roleScopeResource = roleMappingResource.clientLevel(clientUUID);
-                roleScopeResource.add(rolesToAdd);
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    private static List<RoleRepresentation> getRolesToAdd(Set<String> roles, ClientResource clientResource) throws Exception {// todo: test this
-        List<RoleRepresentation> result = new ArrayList<>();
-        RolesResource rolesResource = clientResource.roles();
-        for(String roleName:roles){
-            List<RoleRepresentation> roleRepresentationList = rolesResource.list(roleName,true);
-            if(roleRepresentationList.size() > 1) throw new Exception("multiple roles with name " + roleName + " found");
-            if(roleRepresentationList.size() < 1) throw new Exception(roleName + " not found");
-            result.addAll(roleRepresentationList);
-        }
-        return result;
-    }
-    private static String getClientUUID(String clientID) throws Exception {
-        ClientsResource clientsResource = realmResource.clients();
-        List<ClientRepresentation> clientRepresentationList = clientsResource.findByClientId(clientID);
-        if (clientRepresentationList.size() > 1) {
-            throw new Exception("more than 1 client found");
-        } else if (clientRepresentationList.size() < 1) {
-            throw new Exception("client not found");
-        } else {
-            String clientUUID = clientRepresentationList.get(0).getId();
-            return clientUUID;
-        }
-    }
-
-    private static String getUserID(User user) throws Exception {
-        String result = user.getUserRepresentation().getId();
-        if(result != null) return result;
-        return getUserID(user.getUserRepresentation().getUsername());
-    }
-    private static String getUserID(String userName) throws Exception{
-        List<UserRepresentation> users = usersResource.search(userName);
-        if(users.size() > 1){
-            throw new Exception("error: more than 1 user found");
-        }else if (users.size() == 0){
-            throw new Exception("no users with that username found");
-        } else {
-            return users.get(0).getId();
-        }
-    }
-
-    private static void printUserList() {
+    private static void printKeycloakUserList() {
         List<UserRepresentation> users = usersResource.list();
         System.out.println("list of all users:");
 
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,11 @@
+import org.keycloak.admin.client.CreatedResponseUtil;
+import org.keycloak.admin.client.resource.*;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 import java.util.*;
 
 public class User {
@@ -26,7 +32,7 @@ public class User {
     public void init(){
         userRepresentation = new UserRepresentation();
         userRepresentation.setEnabled(true);
-        clientRoles = new HashMap<String,Set<String>>();
+        clientRoles = new HashMap<>();
     }
 
     public UserRepresentation getUserRepresentation() {
@@ -42,15 +48,103 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    public void addClientRoles(String clientName,String role){
+    public void recordClientRoles(String clientName,String role){
         if (!clientRoles.containsKey(clientName)){
             clientRoles.put(clientName,new HashSet<String>());
         }
         clientRoles.get(clientName).add(role);
     }
 
-    public void removeClientRoles(String clientName,String role){
-        System.out.println("this isnt implemented yet");
-//        todo: implement this
+
+
+    // keycloak methods
+
+    public void addToKeyCloak(UsersResource usersResource) {
+        try {
+            Response response = usersResource.create(userRepresentation);
+            System.out.printf("Response: %s %s%n", response.getStatus(), response.getStatusInfo());
+            System.out.println(response.getLocation());
+            String userId = CreatedResponseUtil.getCreatedId(response);
+            userRepresentation.setId(userId);
+            System.out.printf("User created with userId: %s%n", userId);
+        }catch(WebApplicationException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteFromKeycloak(UsersResource usersResource) {
+        try {
+            String userID = getUserID(usersResource);
+            usersResource.delete(userID);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUserID(UsersResource usersResource) throws Exception {
+        String result = userRepresentation.getId();
+        if (result != null) return result;
+
+        List<UserRepresentation> users = usersResource.search(userRepresentation.getUsername());
+        if (users.size() > 1) {
+            throw new Exception("error: more than 1 user found");
+        } else if (users.size() == 0) {
+            throw new Exception("no users with that username found");
+        } else {
+            return users.get(0).getId();
+        }
+    }
+
+    public void addClientRolesInKeyCloak(RealmResource realmResource) {
+        try{//try looking for the user
+            String userID = getUserID(realmResource.users());
+            for(String clientID: clientRoles.keySet()){
+                try {
+                    String clientUUID = getClientUUID(clientID, realmResource);
+
+                    ClientResource clientResource = realmResource.clients().get(clientUUID);
+                    List<RoleRepresentation> rolesToAdd = getRolesToAdd(clientRoles.get(clientID),clientResource);
+
+                    UserResource userResource = realmResource.users().get(userID);
+                    RoleScopeResource roleScopeResource = userResource.roles().clientLevel(clientUUID);
+
+                    roleScopeResource.add(rolesToAdd);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static String getClientUUID(String clientID,RealmResource realmResource) throws Exception {
+        ClientsResource clientsResource = realmResource.clients();
+        List<ClientRepresentation> clientRepresentationList = clientsResource.findByClientId(clientID);
+        if (clientRepresentationList.size() > 1) {
+            throw new Exception("more than 1 client found");
+        } else if (clientRepresentationList.size() < 1) {
+            throw new Exception("client not found");
+        } else {
+            String clientUUID = clientRepresentationList.get(0).getId();
+            return clientUUID;
+        }
+    }
+
+    private static List<RoleRepresentation> getRolesToAdd(Set<String> roles, ClientResource clientResource){// todo: test this
+        List<RoleRepresentation> result = new ArrayList<>();
+        RolesResource rolesResource = clientResource.roles();
+        for(String roleName:roles){
+            try {
+                List<RoleRepresentation> roleRepresentationList = rolesResource.list(roleName, true);
+                if (roleRepresentationList.size() > 1)
+                    throw new Exception("multiple roles with name " + roleName + " found");
+                if (roleRepresentationList.size() < 1) throw new Exception(roleName + " not found");
+                result.addAll(roleRepresentationList);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
